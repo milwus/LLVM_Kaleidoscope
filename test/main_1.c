7c2e31294ee492686fdbf9db8a3c0aed23462555
milwus <petrickovicm99@gmail.com>
2021-06-12 20:47:31 +0200

    Commited test directory with tests and main.c files

---
+++ test/main_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+double zbir(double, double);
+double kvadrat(double);
+double nula();
+double g(double);
+double anonimna_fja_0();
+double anonimna_fja_1();
+
+int main() {
+	printf("%g\n", zbir(2,4));
+	printf("%g\n", kvadrat(12));
+	printf("%g\n", nula());
+	printf("%g\n", g(3.14));
+	printf("%g\n", anonimna_fja_0());
+	printf("%g\n", anonimna_fja_1());
+
+
+	return 0;
+}
